689ac17e5ea371d8bc1b0c83be26335d2cf92e34
Shaan Vats <shaanv3121@gmail.com>
2025-06-01 11:44:34 -0700

    Create example_usage.py

---
+++ patch_embeddings/example_usage.py
@@ -0,0 +1,11 @@
+# Example usage
+img_size, patch_size, C_in, D = 224, 16, 3, 64
+patch_embeddings = PatchEmbeddings(img_size, patch_size, C_in, D)
+patch_embeddings = patch_embeddings.to(device)
+
+# Forward pass
+B, H_in, W_in = 4, 225, 224
+x = torch.randn(B, C_in, H_in, W_in)
+output = patch_embeddings(x)
+print(output.shape) # Prints shape
+print(output) # Prints tensor
