2b897ddc7202453bb5460f2ad4aeef78e1becac5
Shaan Vats <shaanv3121@gmail.com>
2025-06-01 12:26:53 -0700

    Update example_usage.py

--- patch_embeddings/example_usage.py
+++ patch_embeddings/example_usage.py
@@ -4,7 +4,7 @@ patch_embeddings = PatchEmbeddings(img_size, patch_size, C_in, D)
 patch_embeddings = patch_embeddings.to(device)
 
 # Forward pass
-B, H_in, W_in = 4, 225, 224
+B, H_in, W_in = 4, 225, 224 # H_in value should raise ValueError.
 x = torch.randn(B, C_in, H_in, W_in)
 output = patch_embeddings(x)
 print(output.shape) # Prints shape
